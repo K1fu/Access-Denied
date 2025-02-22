f51035d3f44291800b2b367c51ad254206268041
K1fu <126766655+K1fu@users.noreply.github.com>
2025-02-23 00:14:12 +0800

    Interaction label
    -Added interaction label.
    
    If binabasa niyo ito team, thank you ^^

--- Interact/interact_ray.gd
+++ Interact/interact_ray.gd
@@ -1,5 +1,11 @@
 extends RayCast3D
 
+@onready var prompt = $Label
+
 func _physics_process(_delta):
+	prompt.text = ""
+	
 	if is_colliding():
-		print("COLLISION DETECTED")
+		var collider = get_collider()
+		
+		prompt.text = "DETECTED... " + collider.name
