07c6ec510baab42e19ce384bc01331a05261470a
K1fu <126766655+K1fu@users.noreply.github.com>
2025-03-03 21:46:55 +0800

    Task concept done

--- task_area.gd
+++ task_area.gd
@@ -4,17 +4,21 @@ extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var CrewScreen: Button = $CanvasLayer/Button
 	
 func _ready() -> void:
 	interactable.interact = _on_interact  # Assign function to interaction
 	interactable.connect("area_entered", _on_area_entered)
 	interactable.connect("area_exited", _on_area_exited)
+	CrewScreen.visible = false
 
 func _on_interact():
 	print("Interacted")
+	CrewScreen.visible = true
 
 func _on_area_entered(area: Area2D):
 	print("Entered:", area.name)
 
 func _on_area_exited(area: Area2D):
 	print("Exited:", area.name)
+	CrewScreen.visible = false
