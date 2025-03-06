a9e5a16045d39c19cc131e1e14dfa4a10bf6e123
izoqw <irvineobanel1828@gmail.com>
2025-03-06 20:52:08 +0800

    main menu

---
+++ node_2d_main_menu.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var adress_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/Adress
+
+const Player = preload("res://Scenes/player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("quit"):
+			get_tree().quit()
+
+
+func _on_host_button_pressed():
+	main_menu.hide()
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	
+	add_player(multiplayer.get_unique_id())
+
+func _on_join_button_pressed():
+	main_menu.hide()
+	
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func add_player(peer_id):
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
