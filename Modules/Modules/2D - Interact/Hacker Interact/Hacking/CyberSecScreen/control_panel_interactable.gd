18e7f9dca4e789ebc25625849f0b45897b4df3a2
K1fu <126766655+K1fu@users.noreply.github.com>
2025-05-12 14:58:02 +0800

    added UI

--- Modules/Modules/2D - Interact/Hacker Interact/Hacking/CyberSecScreen/control_panel_interactable.gd
+++ Modules/Modules/2D - Interact/Hacker Interact/Hacking/CyberSecScreen/control_panel_interactable.gd
@@ -2,8 +2,12 @@ extends StaticBody2D
 
 @onready var Dev_interactable: Area2D = $dev_interactable
 @onready var Hack_interactable: Area2D = $hack_interactable
-@onready var ControlPanel: CanvasLayer = $ControlPanel2
-@onready var botton: Button = $ControlPanel2/LobbyBrowsingMenu/LobbyBrowser/VBoxContainer/Button
+@onready var ControlPanel: CanvasLayer = $HackerServer
+@onready var DeveloperPanel: CanvasLayer = $DeveloperPanel
+@onready var botton: Button = $HackerServer/LobbyBrowsingMenu/LobbyBrowser/VBoxContainer/Button
+
+@onready var DevAnimation: AnimationPlayer = $DeveloperPanel/TabAnimation
+@onready var Antivirus: CanvasLayer = $DeveloperPanel
 
 func _ready() -> void:
 	_crewmate_interact()
@@ -11,11 +15,12 @@ func _ready() -> void:
 
 func _crewmate_interact():
 	Dev_interactable.interact = Dev_on_interact  # No parentheses!
-	ControlPanel.visible = false
+	Antivirus.visible = false
 
 func Dev_on_interact():
 	print("Developer Interacted")
-	ControlPanel.visible = true
+	DevAnimation.play("Open tab")
+	Antivirus.visible = true
 
 func _hacker_interact():
 	Hack_interactable.interact = Hack_on_interact  # No parentheses!
