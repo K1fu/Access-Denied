0be3e2b4def8b6c2100ef00e8b42e3da0b08528b
K1fu <126766655+K1fu@users.noreply.github.com>
2025-05-13 06:23:25 +0800

    push

--- Menus/Main Menu/main_menu.gd
+++ Menus/Main Menu/main_menu.gd
@@ -1,28 +1,35 @@
 extends Control
 
 @onready var Glitchityglitch = $"Glitch Transition/VideoStreamPlayer"
+@onready var Sound = $Theme
+@onready var click = $Click
 
 func _ready():
+	Sound.play()
 	Glitchityglitch.visible = false
 	GDSync.connected.connect(connected)
 	GDSync.connection_failed.connect(connection_failed)
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_connect_pressed():
+	click.play()
 	%Connect.disabled = true
 	GDSync.start_multiplayer()
 
 func _on_quit_pressed():
+	click.play()
+	Sound.stop()
 	get_tree().quit()
 
 func connected():
 	%Connect.disabled = true
 	await get_tree().create_timer(0.5).timeout
-
+	
+	Sound.stop()
+	
 	Glitchityglitch.visible = true
 	Glitchityglitch.play()
 	await wait_for_video_end(Glitchityglitch)
-
 	get_tree().change_scene_to_file("res://Scenes/Test environment/game.tscn")
 
 func wait_for_video_end(video_player: VideoStreamPlayer) -> void:
