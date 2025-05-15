a7d71f1cf1a19d8ce47a9819f95a85a8b554db13
K1fu <126766655+K1fu@users.noreply.github.com>
2025-05-16 06:40:11 +0800

    add

--- Modules/Modules/2D - Interact/Hacker Interact/Hacking/sabotage.gd
+++ Modules/Modules/2D - Interact/Hacker Interact/Hacking/sabotage.gd
@@ -3,7 +3,7 @@ extends Button
 @export var cooldownTime: float = 10
 
 @onready var Main: Control = $"../../../../.."
-@onready var HackScreen: CanvasLayer = $".."
+@onready var HackScreen: CanvasLayer = $"../../../../../.."
 @onready var health_bar: ProgressBar = $"../../../CanvasLayer/HealthBar"
 @onready var button: Button = $"."
 
@@ -14,22 +14,22 @@ extends Button
 func _ready() -> void:
 	cooldowntimer.wait_time = cooldownTime
 	cooldownpanel.hide()
+	button.disabled = false
 
 func _on_pressed() -> void:
 	Main.Sabotage()
 	_start_cooldown()
 
-func _start_cooldown():
-	if cooldowntimer.time_left: return
-	
-	cooldowntimer.start()
-	cooldownpanel.show()
+func _start_cooldown() -> void:
+	if cooldowntimer.is_stopped():
+		cooldowntimer.start()
+		cooldownpanel.show()
+		button.disabled = true
 
 func _process(delta: float) -> void:
-	if cooldowntimer.time_left:
-		var roundCooldown = round(cooldowntimer.time_left)
-		if roundCooldown == 0: roundCooldown = 1
-		label.text = str(roundCooldown)
+	if not cooldowntimer.is_stopped():
+		label.text = str(round(cooldowntimer.time_left))
 
 func _on_cooldown_timer_timeout() -> void:
 	cooldownpanel.hide()
+	button.disabled = false
